0d68895097b0f58f178d18cbd6177e813b2f5864
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-08-24 14:48:01 +0600

    Custom components using ES7

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import HelloWorld from './components/HelloWorld'
+import CustomComponents from './components/CustomComponents'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <h1>React+Django</h1>
+      <HelloWorld />
+      <CustomComponents />
     </>
   )
 }
---
+++ src/components/CustomComponents.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomComponents = () => {
+    return (
+        <div>CustomComponents</div>
+    )
+}
+
+export default CustomComponents
