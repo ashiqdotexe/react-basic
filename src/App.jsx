96b08d66b53b67d30e6efb9d51ad54b0a6b8a0e6
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-08-24 20:33:29 +0600

    Props

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import './App.css'
 import HelloWorld from './components/HelloWorld'
 import CustomComponents from './components/CustomComponents'
+import LearnProps from './components/LearnProps'
 function App() {
   const [count, setCount] = useState(0)
-
+  let price = 300
   return (
     <>
       <h1>React+Django</h1>
       <HelloWorld />
       <CustomComponents />
+      <LearnProps stock="Tesla" price={price} />
     </>
   )
 }
---
+++ src/components/LearnProps.jsx
@@ -0,0 +1,12 @@
+
+
+const LearnProps = (props) => {
+    return (
+        <>
+            <h2>Stock name: {props.stock}</h2>
+            <h2>Stock price: {props.price}</h2>
+        </>
+    )
+}
+
+export default LearnProps
