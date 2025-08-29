d44558e298b12d72fc8d6d4b67a58f8e6702ae4e
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-08-29 21:00:30 +0600

    Internal, external CSS and Load image

--- src/components/LearnInlineCss.jsx
+++ src/components/LearnInlineCss.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import "../assets/css/style.css"
+import coveImage from "../assets/images/hello.jpg"
 const LearnInlineCss = () => {
     const style = {
         container: {
@@ -16,8 +17,29 @@ const LearnInlineCss = () => {
     return (
         <>
 
+            <style>
+                {`
+                .container{
+                    background-color:green;
+                    height:100px;
+                    width:100px;
+
+                }
+                
+                `}
+            </style>
+
             <div style={style.container}>
                 <h2 style={style.h2text}>Hello</h2>
+            </div>
+            <hr />
+            <h1>Internal CSS</h1>
+
+            <div className='container'></div>
+            <hr />
+            <h1>External CSS</h1>
+            <div className='imagebox'>
+                <img src={coveImage} alt="" />
             </div>
 
         </>
